d778a625aa6ad1ac36c3bf8434eed308a68e3c3f
Mina-Fathy23 <minafathy2013738@gmail.com>
2026-01-28 02:06:22 +0200

    RTOS default task working successfully

---
+++ qb79616/Debug/FreeRTOS/FreeRTOS/Source/portable/MemMang/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (12.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../FreeRTOS/FreeRTOS/Source/portable/MemMang/heap_1.c 
+
+OBJS += \
+./FreeRTOS/FreeRTOS/Source/portable/MemMang/heap_1.o 
+
+C_DEPS += \
+./FreeRTOS/FreeRTOS/Source/portable/MemMang/heap_1.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+FreeRTOS/FreeRTOS/Source/portable/MemMang/%.o FreeRTOS/FreeRTOS/Source/portable/MemMang/%.su FreeRTOS/FreeRTOS/Source/portable/MemMang/%.cyclo: ../FreeRTOS/FreeRTOS/Source/portable/MemMang/%.c FreeRTOS/FreeRTOS/Source/portable/MemMang/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F103xB -c -I../Core/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/STM32F1xx_HAL_Driver/Inc -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -I"D:/Mina/Racing Team Tasks/2025-2026 Season/BMS_GitHub_Repo/BMS/qb79616/Config" -I"D:/Mina/Racing Team Tasks/2025-2026 Season/BMS_GitHub_Repo/BMS/qb79616/FreeRTOS/FreeRTOS/Source/include" -I"D:/Mina/Racing Team Tasks/2025-2026 Season/BMS_GitHub_Repo/BMS/qb79616/FreeRTOS/FreeRTOS/Source/portable/GCC/ARM_CM3" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-FreeRTOS-2f-FreeRTOS-2f-Source-2f-portable-2f-MemMang
+
+clean-FreeRTOS-2f-FreeRTOS-2f-Source-2f-portable-2f-MemMang:
+	-$(RM) ./FreeRTOS/FreeRTOS/Source/portable/MemMang/heap_1.cyclo ./FreeRTOS/FreeRTOS/Source/portable/MemMang/heap_1.d ./FreeRTOS/FreeRTOS/Source/portable/MemMang/heap_1.o ./FreeRTOS/FreeRTOS/Source/portable/MemMang/heap_1.su
+
+.PHONY: clean-FreeRTOS-2f-FreeRTOS-2f-Source-2f-portable-2f-MemMang
+
