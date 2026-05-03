cb8f33399fe30fb652f759774a1a6d1e2973666d
Mohammed Essam <mohammedessam784@gmail.com>
2026-05-03 14:46:42 +0300

    solved termistor GUI bug

--- qb79616/Core/Src/main.c
+++ qb79616/Core/Src/main.c
@@ -685,10 +685,14 @@ void BMS_MonitorTask(void const * argument)
 			for(uint8_t i = 1; i <= SLAVEBOARDS; i++)
 			{
 				buffer.slave_id = i;
-				float temperature_temp = readGPIOVoltage(i, Thermistor_GPIO, &(GpioReadings[i-1][0]));
+				for(uint8_t j = 1; j <= 4; j++)
+							{
+
+				float temperature_temp = readGPIOVoltage(i, j, &(GpioReadings[i-1][j-1]));
 				buffer.value = (int16_t)(temperature_temp);
 				xQueueSendToBack(bmsMeasurmentsQueue, &buffer, (TickType_t)10);
 				vTaskDelay(pdMS_TO_TICKS(5));
+							}
 			}
 			xTaskNotify(BmsReadTempTaskHandle, NOTIFY_BMS_GOT_TEMP, eSetBits);
 		}
