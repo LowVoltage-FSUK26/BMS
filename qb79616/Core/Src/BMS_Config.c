82efff12a3929e60163039855a16dd33f89815ee
Mina-Fathy23 <minafathy2013738@gmail.com>
2026-06-21 19:35:43 +0300

    charger can IDS added to can message and added charger task, not tested

--- qb79616/Core/Src/BMS_Config.c
+++ qb79616/Core/Src/BMS_Config.c
@@ -6,17 +6,36 @@ extern CAN_HandleTypeDef hcan;
 void BMS_Can_Init(void)
 {
        CAN_FilterTypeDef canFilterConfig;
+       uint32_t filter_id = 0xE5 << 3;      // Compare only ID bits [7:0]
+       uint32_t filter_mask = 0xFF << 3;    // Mask lowest byte only
+
        canFilterConfig.FilterBank = 0;
        canFilterConfig.FilterMode = CAN_FILTERMODE_IDMASK;
        canFilterConfig.FilterScale = CAN_FILTERSCALE_32BIT;
-       canFilterConfig.FilterIdHigh = 0x0000;
-       canFilterConfig.FilterIdLow = 0x0000;
-       //Accept All IDs
-       canFilterConfig.FilterMaskIdHigh = 0x0000;
-       canFilterConfig.FilterMaskIdLow = 0x0000;
+
+       /* Filter ID */
+       canFilterConfig.FilterIdHigh = (filter_id >> 16) & 0xFFFF;
+       canFilterConfig.FilterIdLow  = filter_id & 0xFFFF;
+
+       /* Mask */
+       canFilterConfig.FilterMaskIdHigh = (filter_mask >> 16) & 0xFFFF;
+       canFilterConfig.FilterMaskIdLow  = filter_mask & 0xFFFF;
+
        canFilterConfig.FilterFIFOAssignment = CAN_FILTER_FIFO0;
        canFilterConfig.FilterActivation = ENABLE;
        canFilterConfig.SlaveStartFilterBank = 14;
+
+//       canFilterConfig.FilterBank = 0;
+//       canFilterConfig.FilterMode = CAN_FILTERMODE_IDMASK;
+//       canFilterConfig.FilterScale = CAN_FILTERSCALE_32BIT;
+//       canFilterConfig.FilterIdHigh = 0x0000;
+//       canFilterConfig.FilterIdLow = 0x0000;
+//       //Accept All IDs
+//       canFilterConfig.FilterMaskIdHigh = 0x0000;
+//       canFilterConfig.FilterMaskIdLow = 0x0000;
+//       canFilterConfig.FilterFIFOAssignment = CAN_FILTER_FIFO0;
+//       canFilterConfig.FilterActivation = ENABLE;
+//       canFilterConfig.SlaveStartFilterBank = 14;
 
        if(HAL_CAN_ConfigFilter(&hcan, &canFilterConfig) != HAL_OK)
        {
