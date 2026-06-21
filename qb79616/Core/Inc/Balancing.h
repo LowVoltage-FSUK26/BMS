2156ff49830d030ab8486be626c076df69a1145b
Mohammed Essam <mohammedessam784@gmail.com>
2026-06-21 06:02:30 +0300

    add balancing files

--- qb79616/Core/Inc/Balancing.h
+++ qb79616/Core/Inc/Balancing.h
@@ -8,6 +8,10 @@
 #ifndef INC_BALANCING_H_
 #define INC_BALANCING_H_
 
-
+#define NUM_CELLS            14
+#define BALANCE_DELTA_LSB    105     // ~20mV  (20 / 0.19073)
+#define BAL_CTRL2_RUN        0x33    // AUTO_BAL | BAL_GO | OTCB_EN | FLTSTOP_EN
+#define BAL_CTRL2_STOP       0x00
+#define BAL_LOOP_DELAY_MS    60000   // 60 seconds
 
 #endif /* INC_BALANCING_H_ */
